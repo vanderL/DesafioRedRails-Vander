c373fda104c6abd7628384853071e25da04dfe41
Vander Lima <vnd.vander@gmail.com>
2017-01-12 08:24:16 +0000

    Created Model Post and view with posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
   def profile
@@ -13,9 +14,10 @@ class PagesController < ApplicationController
       # redirect to 404 (root for now)
       redirect_to root_path, :notice=> "User not found!" 
     end
-    
+    @posts = Post.all.where("user_id = ?", User.find(params[:id]))
   end
 
   def explore
+    @posts = Post.all
   end
 end
