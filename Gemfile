b603f16ce739e6e2e33bd3711290e12bc9f0bfaf
Vander Lima <vnd.vander@gmail.com>
2017-01-14 19:57:03 +0000

    Helper devise index page

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
-
+#gem 'sqlite3'
 gem 'pg'
 gem 'rails_12factor', group: :production
 #bootstrap
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # call the configured params 
 
-  
+before_action :configure_permitted_parameters, if: :devise_controller?
   
 
  protected 
